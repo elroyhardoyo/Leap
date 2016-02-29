dad2e1aae777f3b7f77de1755da3fdff1b3d1149
elroyhardoyo <elroy.hardoyo@gmail.com>
2016-02-29 15:44:35 +0700

    first

---
+++ src/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elroy
+ * Date: 2/29/16
+ * Time: 3:39 PM
+ */
+
+
+namespace HelloWorld;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
